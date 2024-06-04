381c9ae5c7feb59a64c36a9e6ca53c138f15573c
Irae Veras <irae.online@gmail.com>
2024-06-04 10:40:11 -0300

    configuração do app e rotas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/users", (req, res) => {
-    res.status(200).send("User")
-})
+app.use(routes);
 
 app.listen(3001, () => {
     console.log("Servidor rodando na porta 3001.");
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,21 @@
 import { Router } from "express";
 
-const routees = Router()
+const routes = Router();
 
-export default router;
+routes.get("/users", (req, res) => {
+    res.status(200).send("Listando usuários")
+});
+routes.get("/users/:id", (req, res) => {
+    res.status(200).send("Listando um usuários")
+});
+routes.post("/users", (req, res) => {
+    res.status(200).send("Inserindo um usuário")
+});
+routes.put("/users/:id", (req, res) => {
+    res.status(200).send("Editando um usuários")
+});
+routes.delete("/users/:id", (req, res) => {
+    res.status(200).send("Excluindo um usuários")
+});
+
+export default routes;
