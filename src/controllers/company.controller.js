ee2c3b90b61346891a3584a8d5a1af4304581c55
Irae Veras <irae.online@gmail.com>
2024-06-04 12:38:59 -0300

    criando rotas do controlador company

---
+++ src/controllers/company.controller.js
@@ -0,0 +1,17 @@
+function Listar(req, res) {
+    res.status(200).send("Listando empresas")
+}
+
+function Inserir(req, res) {
+    res.status(201).send("Inserindo uma empresa")
+}
+
+function Editar(req, res) {
+    res.status(200).send("Editando uma empresa")
+}
+
+function Excluir(req, res) {
+    res.status(200).send("Excluindo uma empresa")
+}
+
+export default { Listar, Inserir, Editar, Excluir}
--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -1,21 +1,12 @@
 import { Router } from "express";
+import companyController from "../controllers/company.controller.js";
 
 const companyRoutes = Router();
 
-companyRoutes.get("/company", (req, res) => {
-    res.status(200).send("Listando empresas")
-});
-companyRoutes.get("/company/:id", (req, res) => {
-    res.status(200).send("Listando uma empresa")
-});
-companyRoutes.post("/company", (req, res) => {
-    res.status(201).send("Inserindo uma empresa")
-});
-companyRoutes.put("/company/:id", (req, res) => {
-    res.status(200).send("Editando uma empresa")
-});
-companyRoutes.delete("/company/:id", (req, res) => {
-    res.status(200).send("Excluindo uma empresa")
-});
+companyRoutes.get("/company", companyController.Listar);
+companyRoutes.get("/company/:id", companyController.Listar);
+companyRoutes.post("/company", companyController.Inserir);
+companyRoutes.put("/company/:id", companyController.Editar);
+companyRoutes.delete("/company/:id", companyController.Excluir);
 
 export default companyRoutes;
