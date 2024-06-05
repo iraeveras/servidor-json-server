515d533268ca3d18064745e365f83ab45ab962bb
Irae Veras <irae.online@gmail.com>
2024-06-05 18:13:15 -0300

    implementando funcionalidade para listar usuários

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,24 @@ import { userBaseURL } from "./scripts/variables/variables.js"
 async function getUser() {
     const response = await fetch(userBaseURL);
     const users = await response.json()
-    console.log(users);
+    return users;
 }
 
-getUser()
+function listUsers() {
+    getUser().then(user => {
+        let listUser = `
+        <p>${user.id}</p>
+        <p>${user.first_name}</p>
+        <p>${user.last_name}</p>
+        <p>${user.email}</p>
+        <p>${user.permission_level}</p>
+        <p>${user.company_id}</p>
+        <p>${user.active}</p>
+        
+        `
+        console.log(listUser);
+    })
+
+}
+
+listUsers()
