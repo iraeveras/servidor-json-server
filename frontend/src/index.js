cec9950db4acb1fc7c24e457ca773add1a909af6
Irae Veras <irae.online@gmail.com>
2024-06-05 22:37:33 -0300

    criação de cadastro de usuário no db

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,81 @@
 'use strict'
-import { userBaseURL } from "./scripts/variables/variables.js"
+import { baseURL } from "./scripts/variables/variables.js";
+import { inputFirstName, inputLastName, inputEmail, inputPemission, inputCompanyId, inputActive, btnCadastrar  } from "./scripts/variables/variables.js";
 
 async function getUser() {
-    const response = await fetch(userBaseURL);
+    const response = await fetch(`${baseURL}/users`);
     const users = await response.json()
     return users;
 }
 
+async function inserirUser(newUser) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    }
+
+    const response = await fetch(`${baseURL}/users`, options)
+    if (response.ok) {
+        console.log("Usuário cadastrado");
+    } else {
+        console.log("Erro ao cadastrar o usuário");
+    }
+}
+
+function registerUser() {
+
+    const dataUser = {
+        first_name: inputFirstName.value,
+        last_name: inputLastName.value,
+        email: inputEmail.value,
+        permission_level: inputPemission.value,
+        company_id: inputCompanyId.value,
+        active: inputActive.value
+    }
+
+
+    if (dataUser.first_name === "") return
+    if (dataUser.last_name === "") return
+    if (dataUser.email === "") return
+    if (dataUser.permission_level === "") return
+    if (dataUser.company_id === "") return
+    if (dataUser.active === "") return
+
+    inserirUser(dataUser)
+}
+
+
+let listUser = ""
+
 function listUsers() {
     getUser().then(user => {
-        let listUser = `
-        <p>${user.id}</p>
-        <p>${user.first_name}</p>
-        <p>${user.last_name}</p>
-        <p>${user.email}</p>
-        <p>${user.permission_level}</p>
-        <p>${user.company_id}</p>
-        <p>${user.active}</p>
-        
-        `
-        console.log(listUser);
+        user.forEach(item => {
+
+            listUser = `
+                <tr>            
+                    <td>${item.id}</td>
+                    <td>${item.first_name}</td>
+                    <td>${item.last_name}</td>
+                    <td>${item.email}</td>
+                    <td>${item.permission_level}</td>
+                    <td>${item.company_id}</td>
+                    <td>${item.active}</td>
+                </tr>
+            
+            `
+            document.getElementById("user-list").innerHTML += listUser
+        })
+
     })
 
 }
 
 listUsers()
+
+btnCadastrar.addEventListener("click", (e) => {
+    e.preventDefault()
+    registerUser()
+})
--- frontend/src/scripts/variables/variables.js
+++ frontend/src/scripts/variables/variables.js
@@ -1,3 +1,19 @@
-const userBaseURL = "http://192.168.2.56:3000/users/";
+const baseURL = "http://localhost:3000";
+const inputFirstName = document.getElementById("first_name");
+const inputLastName = document.getElementById("last_name");
+const inputEmail = document.getElementById("email");
+const inputPemission = document.getElementById("permission_level");
+const inputCompanyId = document.getElementById("company_id");
+const inputActive = document.getElementById("active")
+const btnCadastrar = document.getElementById("btn-cadastrar")
 
-export {userBaseURL}
+export {
+    baseURL,
+    inputFirstName,
+    inputLastName,
+    inputEmail,
+    inputPemission,
+    inputCompanyId,
+    inputActive,
+    btnCadastrar
+}
